1b07c3bfd603737d5bc73dd9d14bb34b766f8e9a
Ximena <a329898@uach.mx>
2021-04-27 16:28:22 -0600

    Introducir una frase por teclado e imprimirla en el centro de la pantalla.

--- p26.js
+++ p26.js
@@ -1,6 +1,7 @@
 console.log("Escribe una frase ");
 var frase = "                                                                 ";
 let longi;
+
 process.stdin.on('data',function(data){
  frase += data.toString();
  console.log(frase);
