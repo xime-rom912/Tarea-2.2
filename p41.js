0e9b9edbe5c9ffea3a7e1c1960afa8f8358f9a80
Ximena <a329898@uach.mx>
2021-04-27 16:37:28 -0600

    Hacer un programa que nos permita dar altas en el archivo DATOS.DAT, cuyos campos son: ID, NOMBRE, APELLIDOS, DIRECCIÓN y ESTADO.

--- p41.js
+++ p41.js
@@ -2,7 +2,9 @@ let campos = [];
 const Data = require('./Data');
 const fs = require('fs');
 let cont = 0;
+
 console.log("Introduzca el ID, nombre, apellidos, direccion y el estado")
+
 process.stdin.on('data',function(data){
  campos[cont] = data.toString().trim();
  if(cont == 4){
